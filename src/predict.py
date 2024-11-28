e2369868c81bde567ce6dd00229728862abd53f1
Helen Truong <htruong122@csu.fullerton.edu>
2024-11-27 16:38:48 -0800

    Create predict.py

---
+++ src/predict.py
@@ -0,0 +1,7 @@
+# Inference script to predict seasonal palette
+
+# def predict_palette(image_path, model):
+#     skin_region = preprocess_image(image_path)
+#     features = extract_features(skin_region)
+#     prediction = model.predict([features])
+#     return prediction
